7368396bc387b4afb3d7cb7a6bb00545a9e64a35
Ariana <136840493+aoverko@users.noreply.github.com>
2024-11-01 19:22:07 -0400

    navbar updates

--- navbar.php
+++ navbar.php
@@ -2,13 +2,22 @@
 //useful thing called Heredoc syntax
 echo <<< HTML
 <nav class="navbar">
-  <div class="container">
-    <a class="navbar-brand" href="landing_page.php">
-      <img src="Logos/lunatech_icon_white.png"  width="30" height="24">
-    </a>
+  <div class="nav-container">
+        <div class="nav-left">
+            <a class="" href="landing_page.php">
+                <img src="Logos/lunatech_icon_white.png" width="6%" margin-left="5%">
+            </a>
+        </div>
+
+        <div class="nav-right">
+            <a class="nav-link" href="catalog.php">Catalog</a>
+            <a class="nav-link" href="cart.php">Cart</a>
+            <a href="">
+                <img src="profile.png" width="6%">
+            </a>
+        </div>
   </div>
 </nav>
 
 HTML;
-
-?> 
+?>
