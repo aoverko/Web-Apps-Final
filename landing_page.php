b6d5e35b6345fce89d102ff62d43c66bfcb6e9a7
Ariana <136840493+aoverko@users.noreply.github.com>
2024-11-04 20:07:25 -0500

    Base Landing Page
    Cool bg and logo :)

--- landing_page.php
+++ landing_page.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="lunatech.css">
 </head>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 <script>
     function loadNavbar() {
         fetch('navbar.php')
@@ -22,16 +22,15 @@
 
 <body onload="loadNavbar()">
     <div id="navbar-area"></div>
-    <div class="vid-bg">
-        <iframe preload="auto"
-            src="SiteAssets/landing_splash.mp4"
-            frameborder="0"
-            allow="autoplay; loop"
-            allowfullscreen>
-        </iframe>
-    </div>
+    <div>
+        <video autoplay muted loop playsinline preload="auto" class="vid-bg">
+            <source src="SiteAssets/wave.mp4" type="video/mp4">
 
+        </video>
+    </div>
+    <div class="landing-logo">
+        <img src="Logos/lunatech_white.png">
+    </div>
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 </html>
