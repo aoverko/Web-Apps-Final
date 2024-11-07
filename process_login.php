f2b830e4ba03efa68d58c81f81ccd02cf5979e0c
NBenson4 <nickjsbenson@outlook.com>
2024-11-07 09:29:36 -0500

    DP Error Fix

--- process_login.php
+++ process_login.php
@@ -6,7 +6,7 @@ $username = 'group1';
 $password = 'tg5z4b31im';
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
